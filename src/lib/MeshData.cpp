23bfd3222006362b22122bfb0eb59fa6cf92abd7
Carl Sandström <carl.sandstrom@chalmers.se>
2015-11-12 13:55:39 +0100

    Added MeshData files

---
+++ src/lib/MeshData.cpp
@@ -0,0 +1,7 @@
+#include "MeshData.h"
+
+MeshData::MeshData()
+{
+
+}
+
---
+++ src/lib/MeshData.h
@@ -0,0 +1,11 @@
+#ifndef MESHDATA_H
+#define MESHDATA_H
+
+
+class MeshData
+{
+public:
+    MeshData();
+};
+
+#endif // MESHDATA_H
