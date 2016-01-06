c9bd8026ba6b7d0ad57fa52e7401639d042537f6
Carl Sandström <sandstrom.carl@gmail.com>
2016-01-06 21:23:35 +0100

    Intermediate

--- src/Examples/TestMeshData.cpp
+++ src/Examples/TestMeshData.cpp
@@ -116,7 +116,6 @@ int main( int argc, char *argv[] ) {
                 EdgeCollapsed=true;
             }
 
-
             if (EdgeCollapsed) {
                 std::ostringstream FileName;
                 FileName << "/tmp/test2D_" << iter << ".vtp";
