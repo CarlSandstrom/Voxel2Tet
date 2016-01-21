3454403a5ce1af81373d22d8b612f0eecced5111
Carl Sandström <sandstrom.carl@gmail.com>
2016-01-21 11:22:45 +0100

    Added intersecting spheres to examples

--- src/Examples/TestCallbackImporter.cpp
+++ src/Examples/TestCallbackImporter.cpp
@@ -8,13 +8,37 @@
 #include "TetGenExporter.h"
 #include "OFFExporter.h"
 
-int GiveMaterialIDByCoordinate(double x, double y, double z)
+int GiveMaterialIDByCoordinateSphere(double x, double y, double z)
 {
 
-    if (sqrt(pow(x-.5, 2) + pow(y-.5, 2) + pow(z-.5, 2)) < .5) {
+    double r=.25;
+
+    if (sqrt(pow(x-.5, 2) + pow(y-.5, 2) + pow(z-.5, 2)) < r) {
         return 1;
     } else {
         return 2;
+    }
+
+}
+
+int GiveMaterialIDByCoordinateTwoSpheres(double x, double y, double z)
+{
+
+    double r=.25;
+    double xc1=0.3, yc1=0.3, zc1=0.3;
+    double xc2=0.3, yc2=0.3, zc2=0.7;
+
+    bool InSphere1 = (sqrt(pow(x-xc1, 2) + pow(y-yc1, 2) + pow(z-zc1, 2)) < r);
+    bool InSphere2 = (sqrt(pow(x-xc2, 2) + pow(y-yc2, 2) + pow(z-zc2, 2)) < r);
+
+    if (InSphere1 & InSphere2) {
+        return 0;
+    } else if (InSphere1) {
+        return 2;
+    } else if (InSphere2) {
+        return 3;
+    } else {
+        return 0;
     }
 
 }
@@ -25,7 +49,12 @@ int main( int argc, char *argv[] )
     voxel2tet::Options *Options = new voxel2tet::Options(argc, argv, DefaultOptions);
 
     voxel2tet::Voxel2Tet v2t(Options);
-    v2t.LoadCallback(&GiveMaterialIDByCoordinate, {0,0,0}, {.1, .1, .1}, {10, 10, 10});
+
+    double spacing=0.1;
+    double length=1.0;
+    int dimensions= std::ceil(length/spacing);
+
+    v2t.LoadCallback(&GiveMaterialIDByCoordinateSphere, {0,0,0}, {spacing, spacing, spacing}, {dimensions, dimensions, dimensions});
     v2t.Process();
     v2t.ExportTetGenFile("/tmp/Voxel2Tet/TetGenTest.poly");
     v2t.ExportOFF("/tmp/Voxel2Tet/TetGenTest.off");
