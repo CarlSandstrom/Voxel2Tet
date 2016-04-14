f0e69128a46ae7cbb1ebe7f2a76203bd6f77d069
Carl Sandström <carl.sandstrom@chalmers.se>
2016-04-14 20:44:29 +0200

    Adding support for linking with TetGen

---
+++ src/lib/TetGenCaller.cpp
@@ -0,0 +1,11 @@
+#include "TetGenCaller.h"
+
+namespace voxel2tet
+{
+
+TetGenCaller::TetGenCaller()
+{
+
+}
+
+}
---
+++ src/lib/TetGenCaller.h
@@ -0,0 +1,16 @@
+#ifndef TETGENCALLER_H
+#define TETGENCALLER_H
+
+#include "tetgen.h"
+
+namespace voxel2tet
+{
+
+class TetGenCaller
+{
+public:
+    TetGenCaller();
+};
+
+}
+#endif // MESHGENERATOR3D_H
