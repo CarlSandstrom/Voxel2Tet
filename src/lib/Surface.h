6fdee89b41d18cdcfc0b39455db71456405de360
Carl Sandström <sandstrom.carl@gmail.com>
2016-05-10 09:24:21 +0200

    Added Doxygen documentation for Surface

--- src/lib/Surface.h
+++ src/lib/Surface.h
@@ -13,34 +13,72 @@ namespace voxel2tet
 {
 
 /**
- * @brief The Surface class
+ * @brief The Surface class holds information and means for manipulation of the surface separating
+ * two materials.
  *
- * One surface object for each pair of materials
+ * One surface object is created for each pair of connected materials. The surface is described using
+ * triangles.
  */
 class Surface
 {
 private:
+    /**
+     * @brief Pointer to an Options object that contains all command line arguments.
+     */
     Options *Opt;
 public:
     std::vector <VertexType*> FixedVertices;
 
+    /**
+     * @brief Constructor for the Surface class
+     * @param Phase1 Phase index on one side of the surface
+     * @param Phase2 Phase index on the other side of the surface
+     * @param Opt Options object for command line arguments and other settings
+     */
     Surface(int Phase1, int Phase2, Options *Opt);
 
+    /**
+     * @brief Holds phase indices for phases on either side of the surface
+     */
     int Phases[2];
+
+    /**
+     * @brief Vertices List of vertices on the surface
+     */
     std::vector <VertexType*> Vertices;
+
+    /**
+     * @brief Triangles List of triangles on the surface
+     */
     std::vector <TriangleType*> Triangles;
+
+    /**
+     * @brief PhaseEdges List of phase edges on the surface
+     */
     std::vector <PhaseEdge*> PhaseEdges;
 
-    /* Moves all internal nodes as if it were a truss structure. This is done in order
-     * to prevent triangles from intersecting during the smoothing phase.
+    /**
+     * @brief AddVertex Adds the vertex \p Vertex to the surface
+     * @param Vertex Vertex to be added to the Surface object
      */
-    void MoveAsTrussStructure();
-
     void AddVertex(VertexType* Vertex);
+
+    /**
+     * @brief AddTriangle Adds a triangle to the surface
+     * @param Triangle Triangle to be added
+     */
     void AddTriangle(TriangleType* Triangle);
 
+    /**
+     * @brief Perform smoothing on this Surface object
+     * @param Mesh
+     */
     void Smooth(MeshData *Mesh);
 
+    /**
+     * @brief Compute area of surface
+     * @return Area of Surface object
+     */
     double ComputeArea();
 };
 
