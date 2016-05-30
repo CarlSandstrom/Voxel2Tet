451036d45037170611701519d9ae2609d7307ae7
Carl Sandström <sandstrom.carl@gmail.com>
2016-05-30 22:15:49 +0200

    Finished export filter for OOFEM

--- src/lib/Export/OOFEMExporter.cpp
+++ src/lib/Export/OOFEMExporter.cpp
@@ -52,16 +52,48 @@ void OOFEMExporter::WriteVolumeData(std::string Filename)
         }
     }
 
+    // Find node sets
+    std::array<double, 3> MaxCoords = {UsedVertices[0]->get_c(0), UsedVertices[0]->get_c(1), UsedVertices[0]->get_c(2) };
+    std::array<double, 3> MinCoords = {UsedVertices[0]->get_c(0), UsedVertices[0]->get_c(1), UsedVertices[0]->get_c(2) };
+
+    for (VertexType *v: UsedVertices) {
+        for (int i=0; i<3; i++) {
+            if (v->get_c(i) > MaxCoords[i]) MaxCoords[i] = v->get_c(i);
+            if (v->get_c(i) < MinCoords[i]) MinCoords[i] = v->get_c(i);
+        }
+    }
+
+    std::vector<VertexType *> xmaxnodes;
+    std::vector<VertexType *> xminnodes;
+    std::vector<VertexType *> ymaxnodes;
+    std::vector<VertexType *> yminnodes;
+    std::vector<VertexType *> zmaxnodes;
+    std::vector<VertexType *> zminnodes;
+
+    // Max and min nodes are arrays of lists of vertices where the index of the array tells in which direction the vertex is located
+    std::array<std::vector<VertexType *>, 3> MaxNodes;
+    std::array<std::vector<VertexType *>, 3> MinNodes;
+
+    double eps = 1e-8;
+
+    for (VertexType *v: UsedVertices) {
+        for (int i=0; i<3; i++) {
+            double cvalue = v->get_c(i);
+            if (fabs(cvalue-MaxCoords[i]) < eps) MaxNodes[i].push_back(v);
+            if (fabs(cvalue-MinCoords[i]) < eps) MinNodes[i].push_back(v);
+        }
+    }
+
     // Write header
 
     OOFEMFile << Filename << ".out\n";
     OOFEMFile << "Exported from Voxel2Tet\n";
-    OOFEMFile << "staticstructural nsteps 1 deltaT 1 lstype 3 smtype 7 rtolf 1.e-12 nmodules 1 maxiter 50\n";
-    OOFEMFile << "vtkxml tstep_step 1 domain_all\n";
+    OOFEMFile << "staticstructural nsteps 1 deltaT 1 lstype 3 smtype 7 rtolf 1.e-8 nmodules 1 maxiter 50\n";
+    OOFEMFile << "vtkxml tstep_all domain_all domain_all primvars 1 1 cellvars 1 1 \n";
     OOFEMFile << "domain 3d\n";
     OOFEMFile << "OutputManager tstep_all dofman_all element_all\n";
     OOFEMFile << "ndofman " << UsedVertices.size() << " nelem " << this->Tets->size() << " ncrosssect 1 nmat " \
-              << Self2OofemMaterials.size() << " nbc 3 nic 1 nltf 1 nset 3 nxfemman 0\n";
+              << Self2OofemMaterials.size() << " nbc 1 nic 1 nltf 1 nset 8 nxfemman 0\n";
 
     // Write vertices
 
@@ -74,8 +106,78 @@ void OOFEMExporter::WriteVolumeData(std::string Filename)
 
     for (size_t i = 0; i<this->Tets->size(); i++) {
         TetType *t = this->Tets->at(i);
-        OOFEMFile << "ltrspace " << i+1 << "\tnodes 4\t" << t->Vertices[0]->tag \
-                  << "\t" << t->Vertices[1]->tag << "\t" << t->Vertices[2]->tag << "\n";
+        OOFEMFile << "ltrspace " << i+1 << "\tnodes 4\t" << t->Vertices[0]->tag+1 \
+                  << "\t" << t->Vertices[1]->tag+1 << "\t" << t->Vertices[2]->tag+1 << "\t" << t->Vertices[3]->tag+1 \
+                  << "\tcrosssect 1 \tmat "<< Self2OofemMaterials[t->MaterialID]<<"\n";
+    }
+
+    // Write cross-section
+    OOFEMFile << "SimpleCS 1 thick 0.1 width 1.0\n";
+
+    // Write Materials
+    i=1;
+    for (auto test: Self2OofemMaterials) {
+        OOFEMFile << "# Material " << test.first << " in source file\n";
+        OOFEMFile << "hyperelmat " << i++ << " d 1 k " << 100 + i*10 << " g " << 100 + i*10 << "\n";
+        // OOFEMFile << "isole " << i++ << " d 1 E 1 n .5 tAlpha 0\n";
+    }
+
+    // Write boundary conditions
+    std::array<double, 3> center = {(MaxCoords[0]+MinCoords[0])/2.0, (MaxCoords[1]+MinCoords[1])/2.0, (MaxCoords[2]+MinCoords[2])/2.0};
+    OOFEMFile << "PrescribedGradient 1 loadTimeFunction 1 ccoord 3 " << center[0] << " " \
+              << center[1] << " " << center[2] << " gradient 3 3 {0.2 0.0 0.0;0.0 0.0 0.0;0.0 0.0 0.0} set 2 dofs 3 1 2 3\n";
+
+    // Write initial condition and load function
+    OOFEMFile << "InitialCondition 1 Conditions 0 set 0\n";
+    OOFEMFile << "ConstantFunction 1 f(t) 1.0\n";
+
+
+    // Write node sets
+    int setid = 1;
+
+    // Volume set
+    OOFEMFile << "# Volume set\n";
+    OOFEMFile << "set " << setid++ << " elements " << this->Tets->size();
+    for (size_t i=0; i<this->Tets->size(); i++) {
+        OOFEMFile << " " << i+1;
+    }
+    OOFEMFile << "\n";
+
+    // Boundary set
+    OOFEMFile << "# Complete boundary set\n";
+    OOFEMFile << "set " << setid++ << " nodes " << (MaxNodes[0].size() + MaxNodes[1].size() + MaxNodes[2].size() + MinNodes[0].size() + MinNodes[1].size() + MinNodes[2].size());
+    for (int i=0; i<3; i++) {
+        // Write max nodes
+        for (size_t j=0; j<MaxNodes[i].size(); j++) {
+            OOFEMFile << " " << MaxNodes[i].at(j)->tag+1;
+        }
+        // Write min nodes
+        for (size_t j=0; j<MinNodes[i].size(); j++) {
+            OOFEMFile << " " << MinNodes[i].at(j)->tag+1;
+        }
+    }
+    OOFEMFile << "\n";
+
+    // Individual max and min sets in all directions
+    std::array<std::string, 3> MaxComments = {"# Max x", "# Max y", "# Max Z"};
+    std::array<std::string, 3> MinComments = {"# Min x", "# Min y", "# Min Z"};
+    for (int i=0; i<3; i++) {
+        // Write max nodes
+        OOFEMFile << MaxComments[i] << "\n";
+        OOFEMFile << "set " << setid++ << " nodes " << MaxNodes[i].size();
+        for (size_t j=0; j<MaxNodes[i].size(); j++) {
+            OOFEMFile << " " << MaxNodes[i].at(j)->tag+1;
+        }
+        OOFEMFile << "\n";
+
+        // Write min nodes
+        OOFEMFile << MinComments[i] << "\n";
+        OOFEMFile << "set " << setid++ << " nodes " << MinNodes[i].size();
+        for (size_t j=0; j<MinNodes[i].size(); j++) {
+            OOFEMFile << " " << MinNodes[i].at(j)->tag+1;
+        }
+        OOFEMFile << "\n";
+
     }
 
 }
