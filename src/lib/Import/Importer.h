2e93d235fb6ef2a922046fd38b0cbbd1f1d1e9c8
Carl Sandström <carl.sandstrom@chalmers.se>
2016-07-07 19:41:33 +0200

    Updated doxygen documentation

--- src/lib/Import/Importer.h
+++ src/lib/Import/Importer.h
@@ -8,6 +8,7 @@
 #include "Options.h"
 
 namespace voxel2tet {
+
 typedef int IntTriplet [ 3 ];
 
 typedef struct {
@@ -19,6 +20,9 @@ typedef struct {
     std :: array< double, 3 >minvalues;
 } BoundingBoxType;
 
+/**
+ * @brief Abstract class for all Importer objects.
+ */
 class Importer
 {
 protected:
@@ -29,7 +33,24 @@ protected:
     int *GrainIdsData;
 
 public:
+
+    /**
+     * @brief Loads data from a file containing a voxel representation.
+     *
+     * As the file is loaded, internal variables for bounding boxes and such are updated. The data is stored in a private variable
+     * and is accessed by GiveMaterialIDByCoordinate and GiveMaterialIDByIndex.
+     *
+     * @param FileName
+     */
     virtual void LoadFile(std :: string FileName) = 0;
+
+    /**
+     * @brief Returns the identifier of the material located at coordinate (x, y, z).
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @param z Z coordinate
+     * @return
+     */
     virtual int GiveMaterialIDByCoordinate(double x, double y, double z);
     virtual int GiveMaterialIDByIndex(int xi, int yi, int zi);
     virtual void GiveSpacing(double spacing [ 3 ]);
