18bf053775c22ae29fd9ce62238f42a1e6aad90a
Carl Sandström <sandstrom.carl@gmail.com>
2016-07-08 16:07:04 +0200

    Fixed CMake files

---
+++ src/bin/MultiSphere.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+#include "Options.h"
+#include "Voxel2Tet.h"
+#include "CallbackImporter.h"
+#include "TetGenExporter.h"
+#include "OFFExporter.h"
+
+int x;
+
+int GiveMaterialIDByCoordinateSphere(double x, double y, double z)
+{
+    double r = .25;
+
+    if ( sqrt( pow(x - .5, 2) + pow(y - .5, 2) + pow(z - .5, 2) ) < r ) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+int GiveMaterialIDByCoordinateTwoSpheres(double x, double y, double z)
+{
+    double r = .25;
+    double xc1 = 0.3, yc1 = 0.3, zc1 = 0.3;
+    double xc2 = 0.3, yc2 = 0.3, zc2 = 0.7;
+
+    bool InSphere1 = ( sqrt( pow(x - xc1, 2) + pow(y - yc1, 2) + pow(z - zc1, 2) ) < r );
+    bool InSphere2 = ( sqrt( pow(x - xc2, 2) + pow(y - yc2, 2) + pow(z - zc2, 2) ) < r );
+
+    if ( InSphere1 & InSphere2 ) {
+        return 0;
+    } else if ( InSphere1 ) {
+        return 2;
+    } else if ( InSphere2 ) {
+        return 3;
+    } else {
+        return 0;
+    }
+}
+
+int GiveMaterialIDByCoordinateBox(double x, double y, double z)
+{
+    if ( ( x > 0.0 ) & ( x < 1.0 ) & ( y > 0.0 ) & ( y < 1.0 ) & ( z > 0.0 ) & ( z < 1.0 ) ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    std :: map< std :: string, std :: string >DefaultOptions;
+    voxel2tet :: Options *Options = new voxel2tet :: Options(argc, argv, DefaultOptions, {});
+
+    voxel2tet :: Voxel2TetClass v2t(Options);
+
+    double spacing = 1.0;
+    double length = 1.0;
+    int dimensions = std :: ceil(length / spacing);
+
+    v2t.LoadCallback(& GiveMaterialIDByCoordinateBox, { { 0, 0, 0 } }, { { spacing, spacing, spacing } }, { { dimensions, dimensions, dimensions } });
+    v2t.Process();
+    v2t.Tetrahedralize();
+}
