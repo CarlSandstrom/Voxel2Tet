d5cd4cfe8887aaab4f681e5d3fb01ceb0765a628
Carl Sandström <sandstrom.carl@gmail.com>
2016-09-19 13:07:45 +0200

    Added Laplace smoother

---
+++ src/lib/Mesh/LaplaceSmoother.h
@@ -0,0 +1,18 @@
+#ifndef LaplaceSmoother_H
+#define LaplaceSmoother_H
+
+#include "SpringSmoother.h"
+
+namespace voxel2tet
+{
+
+class LaplaceSmoother : public SpringSmoother
+{
+public:
+    LaplaceSmoother(double VoxelCharLength, double c, double alpha, double c_factor, bool compute_c=false);
+    ~LaplaceSmoother() {}
+    virtual void Smooth(std :: vector< VertexType * >Vertices, MeshData *Mesh = NULL);
+};
+
+}
+#endif // LaplaceSmoother_H
